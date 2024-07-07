bb3da8d0d41e9c4c9ecdd2d4adda07d444a87cb3
im-sahiljain <mr.sahiljain14@gmail.com>
2024-07-07 21:49:29 +0530

    setting redis client for caching data

--- config.js
+++ config.js
@@ -1,14 +1,25 @@
 const mysql = require('mysql2/promise');
+const redis = require('redis');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-  module.exports = {
-    pool
-  };
-  
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+redisClient.on('error', (err) => {
+  console.log('Redis error: ', err);
+});
+
+module.exports = {
+  pool,
+  redisClient,
+  jwtSecret: process.env.JWT_SECRET,
+};
